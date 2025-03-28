4173b4de5ec00a43964dc5fd49025bd7210591f3
RyougiSama <3238267385@qq.com>
2025-03-28 21:28:08 +0800

    添加项目文件。

---
+++ SignalIOSystem/MainInterface.cpp
@@ -0,0 +1,50 @@
+﻿#include "MainInterface.h"
+
+
+MainInterface::MainInterface(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::MainInterfaceClass())
+{
+    ui->setupUi(this);
+
+    // UI Setup
+    this->updataNoiseDiscription();
+
+    // Connect
+    QObject::connect(ui->comboBox_noise, &QComboBox::currentIndexChanged, this, &MainInterface::updataNoiseDiscription);
+}
+
+MainInterface::~MainInterface()
+{
+    delete ui;
+}
+
+// Updata functons
+void MainInterface::updataNoiseDiscription()
+{
+    ui->textBrowser_NoiseDiscription->setText(
+        this->get_noise_discription(ui->comboBox_noise->currentIndex())
+    );
+}
+
+// Tool functions
+QString MainInterface::get_noise_discription(int noise_id)
+{
+    QString tmp;
+
+    switch (noise_id) {
+    case 0:
+        tmp = "当前没有选择噪声哦~";
+        ui->textBrowser_NoiseDiscription->setFontItalic(true);
+        break;
+    case 1:
+        tmp = "高斯噪声: ";
+        ui->textBrowser_NoiseDiscription->setFontItalic(true);
+        break;
+    default:
+        break;
+    }
+
+    return tmp;
+}
+
---
+++ SignalIOSystem/MainInterface.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include <QtWidgets/QWidget>
+#include "ui_MainInterface.h"
+
+#include <qstring.h>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainInterfaceClass; };
+QT_END_NAMESPACE
+
+class MainInterface : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MainInterface(QWidget *parent = nullptr);
+    ~MainInterface();
+
+private:
+    QString get_noise_discription(int noise_id);
+    void updataNoiseDiscription();
+
+private:
+    Ui::MainInterfaceClass *ui;
+};
