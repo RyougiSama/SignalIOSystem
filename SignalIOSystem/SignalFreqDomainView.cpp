a044bc15185b4803c3bb454c2d8ebd0b4bf4a35a
RyougiSama <3238267385@qq.com>
2025-04-03 13:11:16 +0800

    Add Spectrum Display function

--- SignalIOSystem/SignalFreqDomainView.cpp
+++ SignalIOSystem/SignalFreqDomainView.cpp
@@ -1,5 +1,6 @@
 ﻿#include "SignalFreqDomainView.h"
 #include <qvalueaxis.h>
+#include <qmath.h>
 
 SignalFreqDomainView::SignalFreqDomainView(QWidget *parent)
     : QChartView(parent)
@@ -11,7 +12,7 @@ SignalFreqDomainView::SignalFreqDomainView(QWidget *parent)
     // Create chart and add data series
     auto chart = new QChart();
     chart->setTitleFont(font);
-    chart->setTitle("Frequency-domain Waveform");
+    chart->setTitle("Frequency Spectrum");
     chart->legend()->close();
     chart->addSeries(this->disp_series);
     // Create axes
@@ -20,7 +21,7 @@ SignalFreqDomainView::SignalFreqDomainView(QWidget *parent)
     axisX->setRange(0, 100);
     axisX->setTickCount(11);
     auto axisY = new QValueAxis();
-    axisY->setTitleText("Amplitude");
+    axisY->setTitleText("Magnitude");
     axisY->setRange(0, 1);
     axisY->setTickCount(7);
     // Add axes to chart
@@ -44,12 +45,91 @@ void SignalFreqDomainView::loadSignalData(SignalModel::SignalFileType file_t)
         break;
     case SignalModel::LOAD_FROM_DATA:
         this->sample_rate = this->model->get_signal_sample_rate();
+        this->compute_spectrum();
+        this->disp_spectrum();
         break;
     case SignalModel::LOAD_FROM_CONFIG:
         this->sample_rate = this->model->get_signal_config().sample_rate;
+        this->compute_spectrum();
+        this->disp_spectrum();
         break;
     default:
         break;
+    }
+}
+
+// Tool functions
+void SignalFreqDomainView::compute_spectrum()
+{
+    const auto &raw_data = *this->model->get_signal_raw_data();
+    auto N = raw_data.size();
+    this->freq_resolution = this->sample_rate / N;
+    auto complex_data = zero_padding(raw_data);
+    this->fft(complex_data);
+    this->magnitude.clear();
+    for (int i = 0; i < N / 2 + 1; ++i) {
+        this->magnitude.append(std::abs(complex_data[i]));
+    }
+}
+
+void SignalFreqDomainView::disp_spectrum()
+{
+    // Reset axes
+    // Get current chart
+    auto chart = this->chart();
+    // Update axis X
+    auto axesX = chart->axes(Qt::Horizontal);
+    if (!axesX.isEmpty()) {
+        auto axisX = qobject_cast<QValueAxis *>(axesX.first());
+        if (axisX)
+            axisX->setRange(0, this->sample_rate / 2);
+    }
+    // Update axis Y
+    auto axesY = chart->axes(Qt::Vertical);
+    if (!axesY.isEmpty()) {
+        auto axisY = qobject_cast<QValueAxis *>(axesY.first());
+        if (axisY)
+            axisY->setRange(0, 1.1 * (*std::max_element(this->magnitude.cbegin(), this->magnitude.cend())));
+    }
+    // Draw magnitude
+    this->disp_series->clear();
+    for (int i = 0; i < this->magnitude.size(); ++i) {
+        this->disp_series->append(i * this->freq_resolution, magnitude[i]);
+    }
+    this->update();
+}
+
+QList<SignalFreqDomainView::Complex> SignalFreqDomainView::zero_padding(const QList<double> &raw_data)
+{
+    int padded_size = 1;
+    while (padded_size < raw_data.size()) {
+        padded_size *= 2;
+    }
+    QList<Complex> zero_padded_complex_data(padded_size);
+    for (int i = 0; i < padded_size; ++i) {
+        zero_padded_complex_data[i] = Complex((i < raw_data.size() ? raw_data[i] : 0), 0);
+    }
+    return zero_padded_complex_data;
+}
+
+void SignalFreqDomainView::fft(QList<SignalFreqDomainView::Complex> &complex_data)
+{
+    auto N = complex_data.size();
+    if (N <= 1) return;
+
+    QList<Complex> even(N / 2), odd(N / 2);
+    for (int i = 0; i < N / 2; ++i) {
+        even[i] = complex_data[i * 2];
+        odd[i] = complex_data[i * 2 + 1];
+    }
+
+    fft(even);
+    fft(odd);
+
+    for (int k = 0; k < N / 2; ++k) {
+        Complex t = std::polar(1.0, -2 * M_PI * k / N) * odd[k];
+        complex_data[k] = even[k] + t;
+        complex_data[k + N / 2] = even[k] - t;
     }
 }
 
--- SignalIOSystem/SignalFreqDomainView.h
+++ SignalIOSystem/SignalFreqDomainView.h
@@ -3,11 +3,16 @@
 #include <qchartview.h>
 #include <qchart.h>
 #include <qlineseries.h>
+#include <qlist.h>
+#include <complex>
 #include "SignalModel.h"
 
 class SignalFreqDomainView : public QChartView
 {
     Q_OBJECT
+
+private:
+    using Complex = std::complex<double>;
 
 public:
     SignalFreqDomainView(QWidget *parent);
@@ -17,8 +22,15 @@ public:
     void loadSignalData(SignalModel::SignalFileType file_t);
 
 private:
+    void compute_spectrum();
+    void disp_spectrum();
+    QList<Complex> zero_padding(const QList<double> &raw_data);
+    void fft(QList<Complex> &complex_data);
+
+private:
     const SignalModel *model = nullptr;
-    const QList<double> *disp_spectrum = nullptr;
+    QList<double> magnitude;
     double sample_rate = 0;
+    double freq_resolution = 0;
     QLineSeries *disp_series = nullptr;
 };
